0ef2fa0d41656984f9af9af72aeae84208c1ddf3
Zietieflr <lrmcguire93@gmail.com>
2023-11-23 12:44:28 -0700

    feat: Improve differentiation between components on load.

--- src/app/angular-two-root/root-two/root-two.component.ts
+++ src/app/angular-two-root/root-two/root-two.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root-two',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './root-two.component.html',
   styleUrl: './root-two.component.scss'
 })
-export class RootTwoComponent {
-
-}
+export class RootTwoComponent {}

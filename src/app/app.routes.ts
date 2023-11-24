5412903772fe31992d9a9b37f2400654ff629d1b
Zietieflr <lrmcguire93@gmail.com>
2023-11-23 18:12:40 -0600

    feat: Set up routes in the Angular 1 application. Ensure it can build both directions with the same components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { ChildComponentComponent } from './angular-two-root/child-component/child-component.component';
+import { GrandchildComponent } from './app/grandchild/grandchild.component';
+import { GrandchildTwoComponent } from './app/grandchild-two/grandchild-two.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: 'Angular One',
+    path: '',
+    children: [
+      {
+        path: 'a-one',
+            component: ChildComponentComponent,
+        children: [
+          {
+            path: 'first',
+            component: GrandchildComponent,
+          },
+          {
+            path: 'second',
+            component: GrandchildTwoComponent,
+          }
+        ]
+      }
+    ]
+  }
+];
